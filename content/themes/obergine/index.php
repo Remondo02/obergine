52a5390d2d8361195bc1cea6a8ac1a63bd83bdef
Roromatrix <roromatrix84@gmail.com>
2019-03-26 12:05:17 +0100

    Barre de recherche

--- content/themes/obergine/index.php
+++ content/themes/obergine/index.php
@@ -2,19 +2,14 @@
 
 get_header();
 
-?>
 
-
-
-<?php
-
-
-
-get_search_form();
+get_template_part( 'template-part/searchform' );
 
 get_template_part( 'template-part/searchform-advanced' );
 
 echo('Template page plantes');
+
+
 
 
 $the_query = new WP_Query( $meta_query );
@@ -28,6 +23,26 @@ if ( $the_query->have_posts() ) :
 endif;
 
 wp_reset_postdata();
+
+?>
+
+
+<?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+    <?php if(isset($_GET['post_type'])) {
+        $type = $_GET['post_type'];
+           if($type == 'plant') {
+               
+               the_title();
+               the_content();           
+               
+               } ?>
+    
+<?php } endwhile; else: ?>
+
+ <?= 'Pas de résultats' ?>
+
+<?php endif; 
 
 
 get_footer();
