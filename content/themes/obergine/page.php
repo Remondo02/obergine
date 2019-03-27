8994defb9ffdcfb12e82b2917e2f0836fec3aebe
Gus8617 <florianauguste@gmail.com>
2019-03-27 14:44:14 +0100

    Contact Form Newsletter

--- content/themes/obergine/page.php
+++ content/themes/obergine/page.php
@@ -2,7 +2,18 @@
 get_header();
 ?>
 
-<p>Template page</p>
+<?php
+
+if ( have_posts() ):
+    while ( have_posts() ):
+        the_post();
+        ?>
+        <h3><?= the_title(); ?></h3>
+        <p><?= the_content(); ?></p>
+    <?php
+    endwhile;
+endif;
+?>
 
 <?php
 get_footer();
