8d9dbbe532e6538ced9f4b9591e2302167d96417
remi meullemeestre <remondo02@hotmail.com>
2019-03-29 11:21:37 +0100

    Compte 2.0

--- content/themes/obergine/page-mon-compte.php
+++ content/themes/obergine/page-mon-compte.php
@@ -2,18 +2,27 @@
 get_header();
 ?>
 
-<?php
+<main class="main contact">
+<div class="contact__container">
+        <h1 class="contact__container__title"><?php the_title(); ?></h1>
+        <div class="controls">
 
-if ( have_posts() ):
-    while ( have_posts() ):
-        the_post();
-        ?>
-        <h3><?= the_title(); ?></h3>
-        <p><?= the_content(); ?></p>
-    <?php
-    endwhile;
-endif;
-?>
+            <?php
+            if ( have_posts() ):
+                while ( have_posts() ):
+                    the_post();
+                    ?>
+                    <h3><?= the_title(); ?></h3>
+                    <p><?= the_content(); ?></p>
+                <?php
+                endwhile;
+            endif;
+            ?>
+        </div>
+</div>
+</main>
+
+
 
 <?php
 get_footer();
