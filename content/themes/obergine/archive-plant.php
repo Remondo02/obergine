5da61a3721beb8c8c6a2af2a7a59c02f189e8eb2
Julien Broens <bmf088@gmail.com>
2019-03-29 16:34:07 +0100

    correction d'un bug de compatiblité entre deux versions de github

--- content/themes/obergine/archive-plant.php
+++ content/themes/obergine/archive-plant.php
@@ -20,28 +20,11 @@ get_header();
             endwhile;
         endif;
 
-<<<<<<< HEAD
         wp_reset_postdata();
         ?>
     </section>
     <section class="articles-list-page__main">
         <?php
-=======
-get_template_part( 'template-part/searchform-advanced' );
-
-
-$the_query = new WP_Query( $meta_query );
-
-if ( $the_query->have_posts() ) :
-    while ( $the_query->have_posts() ):
-        $the_query->the_post();
-        the_title();
-        
-    endwhile;
-endif;
-
-wp_reset_postdata();
->>>>>>> 23fa9d2567d93726badbe3ccba03213d110db169
 
 
         $last_post_query = new WP_Query([
