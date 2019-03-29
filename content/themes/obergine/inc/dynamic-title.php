7d22b9f5f5964faccd1175dc2f04eb886c7e3a57
Julien Broens <bmf088@gmail.com>
2019-03-29 13:58:56 +0100

    création de dynamic-title.php

--- content/themes/obergine/inc/dynamic-title.php
+++ content/themes/obergine/inc/dynamic-title.php
@@ -1,25 +1,24 @@
 <?php
 
-        // Dynamisation du titre.
-        
+// Dynamisation du titre.
 
-        $get_title = get_bloginfo();
-        $get_title_array = str_split($get_title );
-        
-        $i = 0;
-        $title1 = [];
-        $title2 = [];
-        
-        foreach ($get_title_array as $key => $value) {
-            if( $i == 0 ) {
-                $i++;
-                $title1[] = $value;
-            } else {
-                $title2[] = $value;
-            }
-        }
-        $part1 = implode($title1);
-        $part2 = implode($title2);
+$get_title = get_bloginfo();
+$get_title_array = str_split($get_title );
 
-        ?>
+$i = 0;
+$title1 = [];
+$title2 = [];
+
+foreach ($get_title_array as $key => $value) {
+    if( $i == 0 ) {
+        $i++;
+        $title1[] = $value;
+    } else {
+        $title2[] = $value;
+    }
+}
+$part1 = implode($title1);
+$part2 = implode($title2);
+
+?>
         
