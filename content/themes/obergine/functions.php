e2eed1e0c5705bdda1bed6e19e6e4e3ef05e821b
Julien Broens <bmf088@gmail.com>
2019-03-29 16:29:38 +0100

    supression d'un require un trop dans function.php

--- content/themes/obergine/functions.php
+++ content/themes/obergine/functions.php
@@ -10,7 +10,7 @@ require get_theme_file_path('inc/customizer.php');
 require get_theme_file_path('inc/search-query.php');
 require get_theme_file_path('inc/widget.php');
 require get_theme_file_path('inc/function-login.php');
-require get_theme_file_path('inc/function-login.php');
+
 
 
 
