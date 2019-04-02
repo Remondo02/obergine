3de90e165469ba82beb8e84ee53640925f817a9b
Julien Broens <bmf088@gmail.com>
2019-04-02 13:00:09 +0200

    reprise de l'intégration de la nav bar mobile de base

--- content/themes/obergine/header.php
+++ content/themes/obergine/header.php
@@ -22,7 +22,6 @@
         <?php
         // Dynamic-title
         require get_theme_file_path('inc/dynamic-title.php');
-
         ?>
 
         
@@ -38,30 +37,36 @@
 
         <!-- Navigation menu burger -->
         <div class="header__header-up__nav">
-            <button class="burger-menu"><i class="fa fa-bars"></i></button>
-        </div>
-
-        <div class="header__header-up__nav d-none">
-            <button class="burger-menu"><i class="fa fa-times"></i></button>
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
+            <nav class="header__header-up__nav navbar navbar-expand-lg navbar-light">
+                <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    
+                    <li class="nav-item">
                     <?php wp_nav_menu( array('theme_location' => 'header-menu') ); ?>
-                </li>
-                <li class="nav-item dropdown">
+                    </li>
+                    <li class="nav-item dropdown">
+                    
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="#">Action</a>
                         <a class="dropdown-item" href="#">Another action</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#">Something else here</a>
                     </div>
-                </li>
-                
-            </ul>
-            <form class="form-inline my-2 my-lg-0" method="get" id="advanced-searchform" role="search" action="<?php echo esc_url( home_url( '/' ) ); ?>">                
-                <input type="hidden" name="search" value="advanced">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" id="searchsubmit" value="Search" type="submit">Search</button>
-            </form>
+                    </li>
+                    
+                </ul>
+                <form class="form-inline my-2 my-lg-0" method="get" id="advanced-searchform" role="search" action="<?php echo esc_url( home_url( '/' ) ); ?>">                
+                    <input type="hidden" name="search" value="advanced">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" id="searchsubmit" value="Search" type="submit">Search</button>
+                </form>
+                </div>
+            </nav>
         </div>
     </div>
 
@@ -83,4 +88,3 @@
         
 </header>
 <body <?php body_class(); ?>>
-
