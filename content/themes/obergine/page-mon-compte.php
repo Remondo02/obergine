b6c799543217f95cc4bc1075c849224ced093a3d
Julien Broens <bmf088@gmail.com>
2019-04-05 11:51:36 +0200

    page mon compte responsive

--- content/themes/obergine/page-mon-compte.php
+++ content/themes/obergine/page-mon-compte.php
@@ -10,17 +10,16 @@ get_header();
             echo do_shortcode('[woocommerce_my_account]');
             
         ?>
-            <a href="<?php echo home_url(); ?>" class="contact__container__link back-home-link">Retour à l'accueil</a>
         </div>       
     </div>
-</main>
-
-
-<footer class='col-3'>
-    <h2 class="h2">Mes favoris</h2>
-    <?php the_user_favorites_list($user_id = null, $site_id = null, $include_links = true, $filters = null, $include_button = false, $include_thumbnails = false, $thumbnail_size = 'thumbnail', $include_excerpt = false);?>
     
-</footer>
+    
+    <div class='fav-bot'>
+        <h2 class="h2">Mes favoris</h2>
+        <?php the_user_favorites_list($user_id = null, $site_id = null, $include_links = true, $filters = null, $include_button = false, $include_thumbnails = false, $thumbnail_size = 'thumbnail', $include_excerpt = false);?>
+        
+    </div>
+</main>
 
 <div class="back-to-home-container back-to-home-container-home-page back-to-home-container-archive-plant">
     <a class="back-to-home-container-link" href="<?= home_url(); ?>" class="main__container__main-container__back-to-home__link">Retourner à l'accueil <i class="fa fa-home" aria-hidden="true"></i></a>
