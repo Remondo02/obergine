db40d9de48880d61a1a985af430e76846ec32ef9
remi meullemeestre <remondo02@hotmail.com>
2019-04-07 11:18:06 +0200

    Construction base pour météo v2 troisième tir

--- content/themes/obergine/app/js/meteo.js
+++ content/themes/obergine/app/js/meteo.js
@@ -15,11 +15,11 @@ $(document).ready(function(){
                 <div class="card-header"><h3>${data.fcst_day_0.day_long}</h3></div>
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-md-1">
+                            <div class="col-md-3" >
                                 <img class="img-thumbnail avatar" src="${data.fcst_day_0.icon_big}">
-                                <span class="btn btn-light btn-block" style="margin-top:.5rem;">${data.current_condition.tmp}°<span>
+                                <span class="btn btn-light btn-block" style="margin-top:.5rem; max-width: 135px;">${data.current_condition.tmp}°<span>
                             </div>
-                            <div class="col-md-9">
+                            <div class="col-md-7">
                                 <span class="badge badge-primary">Température minimale : ${data.fcst_day_0.tmin}</span>
                                 <span class="badge badge-danger">Température maximale : ${data.fcst_day_0.tmax}</span>
                                 <span class="badge badge-info">Vitesse du vent : ${data.current_condition.wnd_spd}</span>
@@ -36,69 +36,38 @@ $(document).ready(function(){
                     </div>
                 </div>
             </div>
+            <br>
 
-                    <div class="d-flex">
-                        <div class="card mb-3" style="max-width: 28%;">
-                            <div class="card-header"><h3>${data.fcst_day_1.day_long}</h3></div>
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-md-3">
-                                            <img class="img-thumbnail avatar" src="${data.fcst_day_1.icon_big}">
-                                            <span class="btn btn-danger btn-block" style="margin-top:.5rem;">${data.fcst_day_1.tmax}°</span>
-                                            <span class="btn btn-primary btn-block">${data.fcst_day_1.tmin}°</span>
-                                        </div>
-                                        <div class="col-md-7">
-                                            <ul class="list-group">
-                                                <li class="list-group-item">Date : ${data.fcst_day_1.date}</li>
-                                                <li class="list-group-item">Condition : ${data.fcst_day_1.condition}</li>
-                                            </ul>
-                                        </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card mb-3" style="max-width: 28%;">
-                            <div class="card-header"><h3>${data.fcst_day_1.day_long}</h3></div>
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-md-3">
-                                            <img class="img-thumbnail avatar" src="${data.fcst_day_1.icon_big}">
-                                            <span class="btn btn-danger btn-block" style="margin-top:.5rem;">${data.fcst_day_1.tmax}°</span>
-                                            <span class="btn btn-primary btn-block">${data.fcst_day_1.tmin}°</span>
-                                        </div>
-                                        <div class="col-md-7">
-                                            <ul class="list-group">
-                                                <li class="list-group-item">Date : ${data.fcst_day_1.date}</li>
-                                                <li class="list-group-item">Condition : ${data.fcst_day_1.condition}</li>
-                                            </ul>
-                                        </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card mb-3" style="max-width: 28%;">
-                            <div class="card-header"><h3>${data.fcst_day_1.day_long}</h3></div>
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-md-3">
-                                            <img class="img-thumbnail avatar" src="${data.fcst_day_1.icon_big}">
-                                            <span class="btn btn-danger btn-block" style="margin-top:.5rem;">${data.fcst_day_1.tmax}°</span>
-                                            <span class="btn btn-primary btn-block">${data.fcst_day_1.tmin}°</span>
-                                        </div>
-                                        <div class="col-md-7">
-                                            <ul class="list-group">
-                                                <li class="list-group-item">Date : ${data.fcst_day_1.date}</li>
-                                                <li class="list-group-item">Condition : ${data.fcst_day_1.condition}</li>
-                                            </ul>
-                                        </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            <div class="card" mb-10" style="max-width: 100rem;">
+        
+              <div class="row" style="justify-content:space-evenly;">
+              
+            <div class="meteo__info">
+            <div class="card-body" style="margin-top:-1.5rem;"><h3>${data.fcst_day_1.day_long}</h3></div>
+                <img id="meteo-image0" style="margin-bottom:1.5rem;"src="${data.fcst_day_1.icon_big}" alt="">
+                <p id="meteo-condition0">${data.fcst_day_1.condition}</p>
+            </div>
+            <div class="meteo__info">
+            <div class="card-body" style="margin-top:-1.5rem;"><h3>${data.fcst_day_2.day_long}</h3></div>
+                <img id="meteo-image0" style="margin-bottom:1.5rem;"src="${data.fcst_day_2.icon_big}" alt="">
+                <p id="meteo-condition0">${data.fcst_day_2.condition}</p>
+            </div>
+            <div class="meteo__info">
+            <div class="card-body" style="margin-top:-1.5rem;"><h3>${data.fcst_day_3.day_long}</h3></div>
+                <img id="meteo-image0" style="margin-bottom:1.5rem;"src="${data.fcst_day_3.icon_big}" alt="">
+                <p id="meteo-condition0">${data.fcst_day_3.condition}</p>
+            </div>
+            <div class="meteo__info">
+            <div class="card-body" style="margin-top:-1.5rem;"><h3>${data.fcst_day_4.day_long}</h3></div>
+                <img id="meteo-image0" style="margin-bottom:1.5rem;"src="${data.fcst_day_4.icon_big}" alt="">
+                <p id="meteo-condition0">${data.fcst_day_4.condition}</p>
+            </div>
+            </div>
+          
+            </div>
+        </div>
+
+
                 
         `);
         });
