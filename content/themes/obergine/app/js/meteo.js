53e368fd517a2a6a781e1e65b5bf436c7ffd5dc7
remi meullemeestre <remondo02@hotmail.com>
2019-04-07 14:46:43 +0200

    Meteo css clean

--- content/themes/obergine/app/js/meteo.js
+++ content/themes/obergine/app/js/meteo.js
@@ -2,22 +2,18 @@ $(document).ready(function(){
     $('#searchWeather').on('keyup', function(e){
       let city = e.target.value;
   
-      // Make request to Github
+      // Fait la requête sur prevision-meteo
         $.ajax({
             url:'https://www.prevision-meteo.ch/services/json/'+city,
-            // data:{
-            //     client_id:'b9315bcd5a07fcd759d8',
-            //     client_secret:'a2b698bf7e7c02f898197cf136d1a41f704ca8e4'
-            // }
         }).done(function(data){
             $('#weather').html(`
-            <div class="card border-primary mb-10" style="max-width: 100rem;">
-                <div class="card-header"><h3>${data.fcst_day_0.day_long}</h3></div>
+            <div class="card border-primary mb-10">
+                <div class="card-header temp-header"><h3>${data.fcst_day_0.day_long}</h3></div>
                     <div class="card-body">
                         <div class="row">
                             <div class="col-md-3" >
                                 <img class="img-thumbnail avatar" src="${data.fcst_day_0.icon_big}">
-                                <span class="btn btn-light btn-block" style="margin-top:.5rem; max-width: 135px;">${data.current_condition.tmp}°<span>
+                                <span class="btn btn-warning btn-block temp-button">${data.current_condition.tmp}°<span>
                             </div>
                             <div class="col-md-7">
                                 <span class="badge badge-primary">Température minimale : ${data.fcst_day_0.tmin}</span>
@@ -27,9 +23,9 @@ $(document).ready(function(){
                                 <span class="badge badge-success">Humidité : ${data.current_condition.humidity}</span>
                                 <br><br>
                                 <ul class="list-group">
-                                <li class="list-group-item">Date : ${data.current_condition.date}</li>
-                                <li class="list-group-item">Heures : ${data.current_condition.hour}</li>
-                                <li class="list-group-item">Condition : ${data.current_condition.condition}</li>
+                                    <li class="list-group-item">Date : ${data.current_condition.date}</li>
+                                    <li class="list-group-item">Heures : ${data.current_condition.hour}</li>
+                                    <li class="list-group-item">Condition : ${data.current_condition.condition}</li>
                                 </ul>
                             </div>
                         </div>
@@ -37,38 +33,31 @@ $(document).ready(function(){
                 </div>
             </div>
             <br>
-
-            <div class="card" mb-10" style="max-width: 100rem;">
-        
-              <div class="row" style="justify-content:space-evenly;">
-              
-            <div class="meteo__info">
-            <div class="card-body" style="margin-top:-1.5rem;"><h3>${data.fcst_day_1.day_long}</h3></div>
-                <img id="meteo-image0" style="margin-bottom:1.5rem;"src="${data.fcst_day_1.icon_big}" alt="">
-                <p id="meteo-condition0">${data.fcst_day_1.condition}</p>
+            <div class="card">
+                <div class="row weather-days-container"> 
+                    <div class="weather-days-container__info">
+                        <div class="card-body temp-day-value"><h3>${data.fcst_day_1.day_long}</h3></div>
+                        <img class="temp-thumbnail" src="${data.fcst_day_1.icon_big}">
+                        <p class="condition">${data.fcst_day_1.condition}</p>
+                    </div>
+                    <div class="weather-days-container__info">
+                        <div class="card-body temp-day-value"><h3>${data.fcst_day_2.day_long}</h3></div>
+                        <img class="temp-thumbnail" src="${data.fcst_day_2.icon_big}">
+                        <p class="condition">${data.fcst_day_2.condition}</p>
+                    </div>
+                    <div class="weather-days-container__info">
+                        <div class="card-body temp-day-value"><h3>${data.fcst_day_3.day_long}</h3></div>
+                        <img class="temp-thumbnail" src="${data.fcst_day_3.icon_big}">
+                        <p class="condition">${data.fcst_day_3.condition}</p>
+                    </div>
+                        <div class="weather-days-container__info">
+                        <div class="card-body temp-day-value"><h3>${data.fcst_day_4.day_long}</h3></div>
+                        <img class="temp-thumbnail" src="${data.fcst_day_4.icon_big}">
+                        <p class="condition">${data.fcst_day_4.condition}</p>
+                    </div>
+                </div>
             </div>
-            <div class="meteo__info">
-            <div class="card-body" style="margin-top:-1.5rem;"><h3>${data.fcst_day_2.day_long}</h3></div>
-                <img id="meteo-image0" style="margin-bottom:1.5rem;"src="${data.fcst_day_2.icon_big}" alt="">
-                <p id="meteo-condition0">${data.fcst_day_2.condition}</p>
-            </div>
-            <div class="meteo__info">
-            <div class="card-body" style="margin-top:-1.5rem;"><h3>${data.fcst_day_3.day_long}</h3></div>
-                <img id="meteo-image0" style="margin-bottom:1.5rem;"src="${data.fcst_day_3.icon_big}" alt="">
-                <p id="meteo-condition0">${data.fcst_day_3.condition}</p>
-            </div>
-            <div class="meteo__info">
-            <div class="card-body" style="margin-top:-1.5rem;"><h3>${data.fcst_day_4.day_long}</h3></div>
-                <img id="meteo-image0" style="margin-bottom:1.5rem;"src="${data.fcst_day_4.icon_big}" alt="">
-                <p id="meteo-condition0">${data.fcst_day_4.condition}</p>
-            </div>
-            </div>
-          
-            </div>
-        </div>
-
-
-                
+        </div>      
         `);
         });
     });
